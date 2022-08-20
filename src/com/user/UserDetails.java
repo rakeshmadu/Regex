7296a218996ab67773171168ae47165043123201
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-20 15:54:48 +0530

    password with minimum 1numeric,including previous uc's

---
+++ src/com/user/UserDetails.java
@@ -0,0 +1,16 @@
+package com.user;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserDetails {
+	
+	final String PASSWORD = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+	
+	public void password(String password) {
+		
+		Pattern pattern = Pattern.compile(PASSWORD);
+		Matcher match = pattern.matcher(password);
+		System.out.println(match.matches());
+	}
+}
